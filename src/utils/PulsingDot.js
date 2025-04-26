f1f2c942db792a6ce3a34374bc5898381966362c
mymac <andikyudo85@gmail.com>
2025-04-26 15:48:56 +0700

    Enhance pulsing dot markers with multi-layer wave animation effect

--- src/utils/PulsingDot.js
+++ src/utils/PulsingDot.js
@@ -1,23 +1,23 @@
-import L from 'leaflet';
+import L from "leaflet";
 
 // Kelas PulsingDot untuk marker kustom
 export class PulsingDot extends L.DivIcon {
-  constructor(options) {
-    const category = options.category || 'default';
-    
-    super({
-      className: `pulsing-dot-container ${category}`,
-      html: `<div class="pulsing-dot ${category}"></div>`,
-      iconSize: [12, 12],
-      iconAnchor: [6, 6],
-      popupAnchor: [0, -6]
-    });
-  }
+	constructor(options) {
+		const category = options.category || "default";
+
+		super({
+			className: `pulsing-dot-container ${category}`,
+			html: `<div class="pulsing-dot ${category}"></div>`,
+			iconSize: [16, 16],
+			iconAnchor: [8, 8],
+			popupAnchor: [0, -8],
+		});
+	}
 }
 
 // Fungsi untuk membuat marker PulsingDot
 export function createPulsingDotMarker(latlng, options = {}) {
-  return L.marker(latlng, {
-    icon: new PulsingDot(options)
-  });
+	return L.marker(latlng, {
+		icon: new PulsingDot(options),
+	});
 }
